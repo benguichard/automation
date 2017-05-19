9c3ac12fcd708e7e1f029f7abcc92c6f64f95ed2
Benjamin Guichard <ben.guichard@live.fr>
2017-05-19 13:32:18 +0000

    Update wordpress ami : move folder

--- wordpress/ami/aws-ubuntu-1604/install.sh
+++ wordpress/ami/aws-ubuntu-1604/install.sh
@@ -5,23 +5,31 @@
 # Create : Variables
 export DEBIAN_FRONTEND=noninteractive
 export CODENAME=`lsb_release --codename | cut -f2`
+export PHP_VERSION=7.0.15-*
+export NGINX_VERSION=1.12.*
 
-# Add : sources repositories
+# add : sources repositories
 wget -qO- https://nginx.org/keys/nginx_signing.key | apt-key add -
 echo "deb http://nginx.org/packages/ubuntu/ $CODENAME nginx" > /etc/apt/sources.list.d/nginx.list
 
-# Update : the system
+# update : system
 apt-get update
-apt-get dist-upgrade -y && apt autoremove -y
+apt-get dist-upgrade -y
+apt-get autoremove -y
 
-# Install : and configure Nginx 1.12
-apt-get install -y nginx=1.12.*
+# install : nginx
+apt-get install -y nginx=$NGINX_VERSION
 sed -i 's/^user.*/user www-data;/' /etc/nginx/nginx.conf
 mv /tmp/default.conf /etc/nginx/conf.d/default.conf
 /usr/sbin/service nginx restart
 
-# Install : Php7 + Mariadb-client
-apt-get install -y php7.0-fpm php7.0-curl php7.0-mcrypt php7.0-gd php7.0-mysql mariadb-client
+# install : php7 + mariadb-client
+apt-get install -y \
+    php7.0-fpm=$PHP_VERSION \
+    php7.0-curl=$PHP_VERSION \
+    php7.0-mcrypt=$PHP_VERSION \
+    php7.0-gd=$PHP_VERSION \
+    php7.0-mysql=$PHP_VERSION
 /usr/sbin/service php7.0-fpm restart
 
 # Install : Wordpress
